47c4114fe31b51acb32fd7d2c8b17ac72e1866b7
Serhii1727 <kravchenko087@ukr.net>
2023-12-17 00:25:32 -0500

    add input Number

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,32 +6,88 @@ export class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
   };
 
-  handleSubmit = evt => {
-    evt.preventDefault();
-    console.log('Hello');
+  handleChangeInput = event => {
+    const { name, value } = event.currentTarget;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState(prevState => ({
+      contacts: [
+        ...prevState.contacts,
+        { name: this.state.name, number: this.state.number, id: nanoid() },
+      ],
+    }));
+
+    this.resetInputForm();
+  };
+
+  resetInputForm = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
   };
 
   render() {
-    const id = nanoid();
-    console.log(id);
+    const { name, number, contacts } = this.state;
+
     return (
       <div className={css.container}>
         <h1 className={css.title}>Phonebook</h1>
         <form className={css.form} onSubmit={this.handleSubmit}>
-          <input
-            className={css.input}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
+          <label className={css.label}>
+            <span>Name</span>
+            <input
+              onChange={this.handleChangeInput}
+              className={css.input}
+              type="text"
+              name="name"
+              value={name}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label className={css.label}>
+            <span>Number</span>
+            <input
+              onChange={this.handleChangeInput}
+              className={css.input}
+              type="tel"
+              name="number"
+              value={number}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
 
-          <button className={css.button}>Add contact</button>
+          <button type="submit" className={css.button}>
+            Add contact
+          </button>
         </form>
         <h1 className={css.title}>Contacts</h1>
+        {contacts.length >= 0 && (
+          <ul>
+            {contacts.map(contact => {
+              return (
+                <li key={contact.id}>
+                  <span>{contact.name}</span>
+                  <span>{contact.number}</span>
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </div>
     );
   }
