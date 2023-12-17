f3ea880cc6f59002621439c1a3de490f8d9027d6
Serhii1727 <kravchenko087@ukr.net>
2023-12-16 20:30:49 -0500

    add murkup step1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import css from './App.module.css';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    console.log('Hello');
+  };
+
+  render() {
+    const id = nanoid();
+    console.log(id);
+    return (
+      <div className={css.container}>
+        <h1 className={css.title}>Phonebook</h1>
+        <form className={css.form} onSubmit={this.handleSubmit}>
+          <input
+            className={css.input}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+
+          <button className={css.button}>Add contact</button>
+        </form>
+        <h1 className={css.title}>Contacts</h1>
+      </div>
+    );
+  }
+}
