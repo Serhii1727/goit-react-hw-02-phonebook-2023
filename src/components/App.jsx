be831c9353b1ecd738af5b12b6f010a39b7fc9a1
Serhii1727 <kravchenko087@ukr.net>
2023-12-17 00:33:27 -0500

    add sumbit form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,15 @@ import css from './App.module.css';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     name: '',
     number: '',
+    filter: '',
   };
 
   handleChangeInput = event => {
@@ -76,6 +82,10 @@ export class App extends Component {
           </button>
         </form>
         <h1 className={css.title}>Contacts</h1>
+        <label className={css.label}>
+          <span>Find contacts by name</span>
+          <input type="text" className={css.input} />
+        </label>
         {contacts.length >= 0 && (
           <ul>
             {contacts.map(contact => {
